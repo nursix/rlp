ac306f2bbf4ad5867d6f0176bd008aa12c6bb1f6
Dominic König <dominic@nursix.org>
2021-03-18 19:47:49 +0100

    Release RLP-1.7.0

---
+++ modules/templates/RLP/upgrade/1.6.5-1.7.0.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+#
+# Database upgrade script
+#
+# RLP Template Version 1.6.5 => 1.7.0
+#
+# Execute in web2py folder after code upgrade like:
+# python web2py.py -S eden -M -R applications/eden/modules/templates/RLP/upgrade/1.6.5-1.7.0.py
+#
+import sys
+#from s3 import S3Duplicate
+
+#from gluon.storage import Storage
+#from gluon.tools import callback
+#from lxml import etree
+
+# Override auth (disables all permission checks)
+auth.override = True
+
+# Failed-flag
+failed = False
+
+# Info
+def info(msg):
+    sys.stderr.write("%s" % msg)
+def infoln(msg):
+    sys.stderr.write("%s\n" % msg)
+
+# Load models for tables
+ltable = s3db.pr_person_availability_site
+
+IMPORT_XSLT_FOLDER = os.path.join(request.folder, "static", "formats", "s3csv")
+TEMPLATE_FOLDER = os.path.join(request.folder, "modules", "templates", "RLP")
+
+# -----------------------------------------------------------------------------
+# Deploy new CMS items
+#
+if not failed:
+    info("Deploy new CMS items")
+
+    # File and Stylesheet Paths
+    stylesheet = os.path.join(IMPORT_XSLT_FOLDER, "cms", "post.xsl")
+    filename = os.path.join(TEMPLATE_FOLDER, "cms_post.csv")
+
+    # Only import relevant CMS posts, do not update any existing ones
+    def cms_post_duplicate(item):
+        name = item.data.get("name")
+        if name in ("Subject OrgToVolunteer",
+                    "Message OrgToVolunteer",
+                    "DelegationOrganizerIntro",
+                    "NotificationIntroOrg",
+                    ):
+            S3Duplicate(noupdate=True)(item)
+        else:
+            item.skip = True
+
+    # Import, fail on any errors
+    try:
+        with open(filename, "r") as File:
+            resource = s3db.resource("cms_post")
+            resource.configure(deduplicate = cms_post_duplicate)
+            resource.import_xml(File,
+                                format = "csv",
+                                stylesheet = stylesheet,
+                                )
+    except:
+        infoln("...failed")
+        infoln(sys.exc_info()[1])
+        failed = True
+    else:
+        if resource.error:
+            infoln("...failed")
+            infoln(resource.error)
+            failed = True
+        else:
+            infoln("...done")
+
+# -----------------------------------------------------------------------------
+# Upgrade user roles
+#
+if not failed:
+    info("Upgrade user roles")
+
+    bi = s3base.S3BulkImporter()
+    filename = os.path.join(TEMPLATE_FOLDER, "auth_roles.csv")
+
+    with open(filename, "r") as File:
+        try:
+            bi.import_role(filename)
+        except Exception as e:
+            infoln("...failed")
+            infoln(sys.exc_info()[1])
+            failed = True
+        else:
+            infoln("...done")
+
+# -----------------------------------------------------------------------------
+# Finishing up
+#
+if failed:
+    db.rollback()
+    infoln("UPGRADE FAILED - Action rolled back.")
+else:
+    db.commit()
+    infoln("UPGRADE SUCCESSFUL.")
